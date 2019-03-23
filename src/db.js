99b59b9fd21c719370c3958653f0427f161322cc
Narendras <n.syahrasyad.n@gmail.com>
2019-03-23 13:40:27 -0700

    Updated to use pg promise

---
+++ src/db.js
@@ -0,0 +1,6 @@
+import pgp from 'pg-promise';
+import { connectionOptions } from '../config';
+
+const init = pgp();
+
+export default init(connectionOptions);
--- src/queries.js
+++ src/queries.js
@@ -1,18 +1,11 @@
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'password',
-  port: 5432
-});
+import db from './db';
 
 export const get = (_, res) => {
-  pool.query(`select * from test`, (error, results) => {
-    if (error) {
+  db.any('select * from test')
+    .then(data => {
+      res.status(200).json({ data });
+    })
+    .catch(error => {
       res.status(400).json({ error });
-    }
-    res.status(200).json({ data: results.rows });
-  });
+    });
 };
