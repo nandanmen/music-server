f54d5109e9332aacc89e9c3a0c7c30cf4a7d3242
Narendras <n.syahrasyad.n@gmail.com>
2019-03-25 20:10:37 -0700

    Updated handlers to reflect db

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,94 +1,48 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import db from './db';
 
-const secret = process.env.SECRET;
-const exp = process.env.EXP;
-
-/**
- * Creates a JSON web token from the given id.
- * @param {string} id
- * @returns {string}
- */
-export const makeToken = id => jwt.sign({ id }, secret, { expiresIn: exp });
-
-/**
- * Checks if the given token is valid. If it is,
- * return the payload.
- * @param {string} token
- * @returns {Promise<string>}
- */
-export const verifyToken = token =>
-  new Promise((resolve, reject) => {
-    jwt.verify(token, secret, (error, payload) => {
-      if (error) return reject(error);
-      resolve(payload.id);
-    });
-  });
-
-/**
- * Checks if the given password matches the hashed
- * password.
- * @param {string} hashed
- * @param {string} password
- * @returns {Promise<boolean>}
- */
-export const compare = (hashed, password) =>
-  new Promise((resolve, reject) => {
-    bcrypt.compare(password, hashed, (err, same) => {
-      if (err) return reject(err);
-      resolve(same);
-    });
-  });
-
-export const registerHandler = (req, res) => {
-  const { email, password, name, type } = req.body;
-  if (!email || !password || !name || !type) {
+export const registerHandler = async (req, res) => {
+  const { id, username, password, name, type } = req.body;
+  if (!id || !username || !password || !name || !type) {
     return res
       .status(400)
-      .json({ error: 'Email, password, name and type are required' });
+      .json({ error: 'ID, username, password, name and type are required' });
   }
-  bcrypt
-    .hash(password, 8)
-    .then(hashed => {
-      db.one(
-        'insert into usertest (name, email, password) values ($1, $2, $3) returning id',
-        [name, email, hashed]
-      )
-        .then(({ id }) => res.status(201).json({ token: makeToken(id) }))
-        .catch(error => {
-          console.error(error);
-          res.status(500).json({ error });
-        });
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).json({ error });
-    });
+  try {
+    await db.none('insert into user_2 (user_id, username) values ($1, $2)', [
+      id,
+      username
+    ]);
+    await db.none(
+      'insert into user_1 (user_id, name, password) values ($1, $2, $3)',
+      [id, name, password]
+    );
+    if (type === 'artist') {
+      await db.none('insert into artist (user_id) values ($1)', [id]);
+    }
+    res.status(201).json({ id });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 export const loginHandler = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ error: 'Email and password is required' });
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password is required' });
   }
 
   try {
     const user = await db.oneOrNone(
-      'select id, password from usertest where email = $1',
-      [email]
+      'select u1.user_id, password from user_1 u1, user_2 u2 where u1.user_id = u2.user_id and u2.username = $1',
+      [username]
     );
     if (!user) {
-      return res.status(404).json({ error: 'That email does not exist' });
+      return res.status(404).json({ error: 'That username does not exist' });
     }
-
-    const hashed = user.password.trim();
-    const authorized = await compare(hashed, password);
-    if (!authorized) {
+    if (!(user.password === password)) {
       return res.status(401).json({ error: 'Incorrect password' });
     }
-
-    res.status(200).json({ token: makeToken(user.id) });
+    res.status(200).json({ id: user.user_id });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error });
@@ -124,7 +78,7 @@ export const protectRoutesHandler = async (req, res, next) => {
 };
 
 export const authorizeArtist = async (req, res, next) => {
-  const { id } = req.user;
+  const { id } = req.query;
   try {
     const data = await db.oneOrNone('select * from artist where user_id = $1', [
       id
